383fd8f78c1d86841fa57645454663a6e8a04646
Agnieszka K. Kocinski <0robotzero@gmail.com>
2019-09-08 20:29:14 +0100

    cicle radious

--- src/main/java/com/robotzero/gamefx/GameApp.java
+++ src/main/java/com/robotzero/gamefx/GameApp.java
@@ -4,6 +4,8 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.sslogger.Logger;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
@@ -15,8 +17,11 @@ public class GameApp extends GameApplication {
     private Vec2D two = new Vec2D(0, 540);
     private Line line = new Line();
     private Rectangle rectangle = new Rectangle();
+    private Circle circle = new Circle();
     private Vec2D newV = new Vec2D(two.getX(), two.getY());
+    private Vec2D circleCenter = new Vec2D(1920 / 2, 1080 / 2);
     private Pane pane;
+    private double theta = 0;
 
     @Override
     protected void initSettings(GameSettings gameSettings) {
@@ -28,26 +33,43 @@ public class GameApp extends GameApplication {
         rectangle.setWidth(10);
         rectangle.setX(0);
         rectangle.setY(540);
-        line.setStartX(one.getX());
-        line.setStartY(one.getY());
-        line.setEndX(two.getX());
-        line.setEndY(two.getY());
-        pane = new Pane(line, rectangle);
+        line.setStartX(1920 / 2);
+        line.setStartY(1080 / 2);
+        line.setEndX(1920 /2 + 520 );
+        line.setEndY(540);
+        line.setStroke(Color.RED);
+        line.setStrokeWidth(5);
+        circle.setRadius(520);
+        circle.setCenterX(1920 / 2);
+        circle.setCenterY(1080 / 2);
+        circle.setStrokeWidth(5);
+        circle.setStroke(Color.BLACK);
+
+        pane = new Pane(rectangle, circle, line);
     }
 
     @Override
     protected void onUpdate(double tpf) {
         double ax, by;
         ax = this.newV.getX();
+//        float x = width / 2 + cos(a) * r;
+//        float y = height / 2 + sin(a) * r;
         int bb = (int) (100 * Math.sin(0.2 * ax / 3.14));
         Vec2D norm = this.normalizeCoord(this.newV);
+        Vec2D circleNorm = this.normalizeCoord(this.circleCenter);
         Vec2D sins = new Vec2D(norm.getX(), 1 * Math.sin(norm.getX() * 100 / Math.PI));
+        Vec2D circleNorm2 = new Vec2D(this.circleCenter.getX() + Math.cos(theta) * 520, this.circleCenter.getY() + Math.sin(theta) * 520);
         Vec2D backToScreen = toScreenCoord(sins);
+        Vec2D backToScreenCirle = toScreenCoord(circleNorm2);
         this.newV = new Vec2D(backToScreen.getX() + 1, backToScreen.getY());
+        theta = theta + tpf;
         try {
             Thread.sleep(10);
             rectangle.setX(backToScreen.getX());
             rectangle.setY(backToScreen.getY());
+            line.setEndX(circleNorm2.getX());
+            line.setEndY(circleNorm2.getY());
+            System.out.println(this.normalizeCoord(this.circleCenter));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
