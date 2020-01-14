10ea187bc4252f8ea0d4cf8f61a7c201c4f06ade
Agnieszka K. Kocinski <0robotzero@gmail.com>
2020-01-14 21:25:11 +0000

    Texture things without pixel manipulation

--- src/main/java/com/robotzero/gamefx/renderengine/utils/FileUtils.java
+++ src/main/java/com/robotzero/gamefx/renderengine/utils/FileUtils.java
@@ -1,8 +1,19 @@
 package main.java.com.robotzero.gamefx.renderengine.utils;
 
+import org.lwjgl.system.MemoryUtil;
+
+import org.lwjgl.BufferUtils;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 final public class FileUtils {
 
@@ -24,4 +35,41 @@ final public class FileUtils {
         return result.toString();
     }
 
+    public static ByteBuffer ioResourceToByteBuffer(String resource, int bufferSize) throws IOException {
+        ByteBuffer buffer;
+
+        Path path = Paths.get(resource);
+        if (Files.isReadable(path)) {
+            try (SeekableByteChannel fc = Files.newByteChannel(path)) {
+                buffer = MemoryUtil.memAlloc((int) fc.size() + 1);
+                while (fc.read(buffer) != -1) ;
+            }
+        } else {
+            try (
+                    InputStream source = FileUtils.class.getResourceAsStream(resource);
+                    ReadableByteChannel rbc = Channels.newChannel(source)) {
+                buffer = MemoryUtil.memAlloc(bufferSize);
+
+                while (true) {
+                    int bytes = rbc.read(buffer);
+                    if (bytes == -1) {
+                        break;
+                    }
+                    if (buffer.remaining() == 0) {
+                        buffer = resizeBuffer(buffer, buffer.capacity() * 2);
+                    }
+                }
+            }
+        }
+
+        buffer.flip();
+        return buffer;
+    }
+
+    private static ByteBuffer resizeBuffer(ByteBuffer buffer, int newCapacity) {
+        ByteBuffer newBuffer = BufferUtils.createByteBuffer(newCapacity);
+        buffer.flip();
+        newBuffer.put(buffer);
+        return newBuffer;
+    }
 }
--- src/main/java/com/robotzero/gamefx/renderengine/utils/Texture.java
+++ src/main/java/com/robotzero/gamefx/renderengine/utils/Texture.java
@@ -1,54 +1,101 @@
 package main.java.com.robotzero.gamefx.renderengine.utils;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.FileInputStream;
+import org.lwjgl.system.MemoryStack;
+
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import static org.lwjgl.system.MemoryStack.stackPush;
 
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.stb.STBImage.stbi_image_free;
+import static org.lwjgl.stb.STBImage.stbi_load_from_memory;
 
 public class Texture {
     private int width, height;
-    private int texture;
+//    private int texture;
+    private int id;
 
     public Texture(String path) {
-        texture = load(path);
-    }
-
-    private int load(String path) {
-        int[] pixels;
+//        texture = load(path);
         try {
-            BufferedImage image = ImageIO.read(new FileInputStream(path));
-            width = image.getWidth();
-            height = image.getHeight();
-            pixels = new int[width * height];
-            image.getRGB(0, 0, width, height, pixels, 0, width);
+            ByteBuffer blah =  FileUtils.ioResourceToByteBuffer(path, 1024);
+            load2(blah);
         } catch (IOException e) {
             e.printStackTrace();
-            throw new RuntimeException(e.getMessage());
         }
-
-        int[] data = new int[width * height];
-            for (int i = 0; i < width * height; i++) {
-                int a = (pixels[i] & 0xff000000) >> 24;
-                int r = (pixels[i] & 0xff0000) >> 16;
-                int g = (pixels[i] & 0xff00) >> 8;
-                int b = (pixels[i] & 0xff);
-
-                data[i] = a << 24 | b << 16 | g << 8 | r;
-            }
-
-        int result = glGenTextures();
-        glBindTexture(GL_TEXTURE_2D, result);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, BufferUtils.createIntBuffer(data));
-        glBindTexture(GL_TEXTURE_2D, 0);
-        return result;
     }
 
+    private void load2(ByteBuffer imageData) {
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer w = stack.mallocInt(1);
+            IntBuffer h = stack.mallocInt(1);
+            IntBuffer avChannels = stack.mallocInt(1);
+
+            // Decode texture image into a byte buffer
+            ByteBuffer decodedImage = stbi_load_from_memory(imageData, w, h, avChannels, 4);
+
+            this.width = w.get();
+            this.height = h.get();
+
+            // Create a new OpenGL texture
+            this.id = glGenTextures();
+            // Bind the texture
+            glBindTexture(GL_TEXTURE_2D, this.id);
+
+            // Tell OpenGL how to unpack the RGBA bytes. Each component is 1 byte size
+            glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            // Upload the texture data
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this.width, this.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, decodedImage);
+            // Generate Mip Map
+//            glGenerateMipmap(GL_TEXTURE_2D);
+
+            stbi_image_free(imageData);
+        }
+    }
+
+
+//    private int load(String path) {
+//        int[] pixels;
+//        try {
+//            BufferedImage image = ImageIO.read(new FileInputStream(path));
+//            width = image.getWidth();
+//            height = image.getHeight();
+//            pixels = new int[width * height];
+//            image.getRGB(0, 0, width, height, pixels, 0, width);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            throw new RuntimeException(e.getMessage());
+//        }
+//
+//        int[] data = new int[width * height];
+//            for (int i = 0; i < width * height; i++) {
+//                int a = (pixels[i] & 0xff000000) >> 24;
+//                int r = (pixels[i] & 0xff0000) >> 16;
+//                int g = (pixels[i] & 0xff00) >> 8;
+//                int b = (pixels[i] & 0xff);
+//
+//                data[i] = a << 24 | b << 16 | g << 8 | r;
+//            }
+//
+//        int result = glGenTextures();
+//        glBindTexture(GL_TEXTURE_2D, result);
+//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+//        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, BufferUtils.createIntBuffer(data));
+//        glBindTexture(GL_TEXTURE_2D, 0);
+//        return result;
+//    }
+
+//    public void bind() {
+//        glBindTexture(GL_TEXTURE_2D, texture);
+//    }
+
     public void bind() {
-        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindTexture(GL_TEXTURE_2D, this.id);
     }
 
     public void unbind() {
@@ -64,6 +111,6 @@ public class Texture {
     }
 
     public void cleanup() {
-        glDeleteTextures(texture);
+        glDeleteTextures(this.id);
     }
 }
