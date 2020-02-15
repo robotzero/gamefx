04e86e35f7135f2181e3729c8aab204fc33086a8
Agnieszka K. Kocinski <0robotzero@gmail.com>
2020-02-15 14:41:15 +0000

    day 44 done

--- src/main/java/com/robotzero/gamefx/renderengine/Player.java
+++ src/main/java/com/robotzero/gamefx/renderengine/Player.java
@@ -1,6 +1,7 @@
 package com.robotzero.gamefx.renderengine;
 
 import com.robotzero.gamefx.world.TileMap;
+import imgui.Col;
 import org.joml.Matrix4f;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
@@ -49,15 +50,46 @@ public class Player {
         PlayerRight.Offset.x += 0.5f*PlayerWidth;
         PlayerRight = tileMap.RecanonicalizePosition(PlayerRight);
 
-
-        if (tileMap.IsWorldPointEmpty(NewPlayerP) &&
-            tileMap.IsWorldPointEmpty(PlayerLeft) &&
-            tileMap.IsWorldPointEmpty(PlayerRight))
-        {
-            positionc = new TileMap.TileMapPosition();
-            positionc.AbsTileX = NewPlayerP.AbsTileX;
-            positionc.AbsTileY = NewPlayerP.AbsTileY;
-            positionc.Offset = NewPlayerP.Offset;
+        boolean Collided = false;
+        TileMap.TileMapPosition colP = new TileMap.TileMapPosition();
+        if (!tileMap.IsWorldPointEmpty(NewPlayerP)) {
+            colP = NewPlayerP;
+            Collided = true;
         }
+
+        if (!tileMap.IsWorldPointEmpty(PlayerLeft)) {
+            colP = PlayerLeft;
+            Collided = true;
+        }
+
+        if (!tileMap.IsWorldPointEmpty(PlayerRight)) {
+            colP = PlayerRight;
+            Collided = true;
+        }
+
+        if (Collided)
+        {
+            var reflection = new Vector2f(0f, 0f);
+            if (colP.AbsTileX < Player.positionc.AbsTileX) {
+                reflection = new Vector2f(1f, 0f);
+            }
+
+            if (colP.AbsTileX > Player.positionc.AbsTileX) {
+                reflection = new Vector2f(-1f, 0f);
+            }
+
+            if (colP.AbsTileY < Player.positionc.AbsTileY) {
+                reflection = new Vector2f(0f, 1f);
+            }
+
+            if (colP.AbsTileY > Player.positionc.AbsTileY) {
+                reflection = new Vector2f(0f, -1f);
+            }
+
+            Player.dPlayerP = new Vector2f(dPlayerP.x(), dPlayerP.y()).sub(new Vector2f(reflection.x(), reflection.y()).mul(new Vector2f(dPlayerP.x(), dPlayerP.y()).dot(reflection)));
+        } else {
+            Player.positionc = NewPlayerP;
+        }
+
     }
 }
